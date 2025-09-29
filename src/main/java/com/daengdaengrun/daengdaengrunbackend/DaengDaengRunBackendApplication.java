abdcfaffcca38f517a4367740c71af9071632788
이경준 <kyoungjun7777@naver.com>
2025-09-29 21:52:36 +0900

    fin

--- src/main/java/com/daengdaengrun/daengdaengrunbackend/DaengDaengRunBackendApplication.java
+++ src/main/java/com/daengdaengrun/daengdaengrunbackend/DaengDaengRunBackendApplication.java
@@ -1,5 +1,6 @@
 package com.daengdaengrun.daengdaengrunbackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class DaengDaengRunBackendApplication {
 
     public static void main(String[] args) {
+        // .env 파일을 로드해서 시스템 프로퍼티(System properties)로 설정합니다.
+        Dotenv dotenv = Dotenv.configure().systemProperties().load();
+
         SpringApplication.run(DaengDaengRunBackendApplication.class, args);
     }
 
